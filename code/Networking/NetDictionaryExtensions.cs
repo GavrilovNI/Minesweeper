4ecc5020d75ac461e36053c4646ee0ef1543e839
Nikita Gavrilov <pelmeshka1110@gmail.com>
2024-04-01 05:26:59 +0200

    feat: add some extension methods for NetList & NetDictionary

---
+++ code/Networking/NetDictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace Minesweeper.Networking;
+
+public static class NetDictionaryExtensions
+{
+    public static TValue GetValueOrDefault<TKey, TValue>(this NetDictionary<TKey, TValue> dictionary, TKey key, TValue? defaultValue = default)
+    {
+        if(dictionary.TryGetValue(key, out var value))
+            return value;
+        return defaultValue!;
+    }
+
+    public static IEnumerable<KeyValuePair<TKey, TValue>> ToEnumerable<TKey, TValue>(this NetDictionary<TKey, TValue> dictionary)
+    {
+        foreach(var item in dictionary)
+            yield return item;
+    }
+}
---
+++ code/Networking/NetListExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace Minesweeper.Networking;
+
+public static class NetListExtensions
+{
+    public static IEnumerable<T> ToEnumerable<T>(this NetList<T> list)
+    {
+        foreach(var item in list)
+            yield return item;
+    }
+}
